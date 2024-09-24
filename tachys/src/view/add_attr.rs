c28955b56899f8791fa6bb8dc8f7958ed8e6763e
Greg Johnston <greg.johnston@gmail.com>
2024-09-23 20:43:48 -0400

    remove generic rendering

--- tachys/src/view/add_attr.rs
+++ tachys/src/view/add_attr.rs
@@ -8,34 +8,28 @@ use crate::{html::attribute::Attribute, renderer::Renderer};
 /// Normally, this is used to add an attribute to an HTML element. But it is required to be
 /// implemented for all types that implement [`RenderHtml`], so that attributes can be spread onto
 /// other structures like the return type of a component.
-pub trait AddAnyAttr<Rndr>
-where
-    Rndr: Renderer,
-{
+pub trait AddAnyAttr {
     /// The new type once the attribute has been added.
-    type Output<SomeNewAttr: Attribute<Rndr>>: RenderHtml<Rndr>;
+    type Output<SomeNewAttr: Attribute>: RenderHtml;
 
     /// Adds an attribute to the view.
-    fn add_any_attr<NewAttr: Attribute<Rndr>>(
+    fn add_any_attr<NewAttr: Attribute>(
         self,
         attr: NewAttr,
     ) -> Self::Output<NewAttr>
     where
-        Self::Output<NewAttr>: RenderHtml<Rndr>;
+        Self::Output<NewAttr>: RenderHtml;
 }
 
 /// Declares that spreading attributes onto a particular type has no effect.
 #[macro_export]
 macro_rules! no_attrs {
     ($ty_name:ty) => {
-        impl<'a, R> $crate::view::add_attr::AddAnyAttr<R> for $ty_name
-        where
-            R: Renderer,
-        {
-            type Output<SomeNewAttr: $crate::html::attribute::Attribute<R>> =
+        impl<'a> $crate::view::add_attr::AddAnyAttr for $ty_name {
+            type Output<SomeNewAttr: $crate::html::attribute::Attribute> =
                 $ty_name;
 
-            fn add_any_attr<NewAttr: $crate::html::attribute::Attribute<R>>(
+            fn add_any_attr<NewAttr: $crate::html::attribute::Attribute>(
                 self,
                 _attr: NewAttr,
             ) -> Self::Output<NewAttr> {
@@ -45,37 +39,35 @@ macro_rules! no_attrs {
     };
 }
 
-impl<T, Rndr> AddAnyAttr<Rndr> for BoxedView<T>
+impl<T> AddAnyAttr for BoxedView<T>
 where
-    T: AddAnyAttr<Rndr> + Send,
-    Rndr: Renderer,
+    T: AddAnyAttr + Send,
 {
-    type Output<SomeNewAttr: Attribute<Rndr>> = T::Output<SomeNewAttr>;
+    type Output<SomeNewAttr: Attribute> = T::Output<SomeNewAttr>;
 
-    fn add_any_attr<NewAttr: Attribute<Rndr>>(
+    fn add_any_attr<NewAttr: Attribute>(
         self,
         attr: NewAttr,
     ) -> Self::Output<NewAttr>
     where
-        Self::Output<NewAttr>: RenderHtml<Rndr>,
+        Self::Output<NewAttr>: RenderHtml,
     {
         self.into_inner().add_any_attr(attr)
     }
 }
 
-impl<T, Rndr> AddAnyAttr<Rndr> for WrappedView<T>
+impl<T> AddAnyAttr for WrappedView<T>
 where
-    T: AddAnyAttr<Rndr> + Send,
-    Rndr: Renderer,
+    T: AddAnyAttr + Send,
 {
-    type Output<SomeNewAttr: Attribute<Rndr>> = T::Output<SomeNewAttr>;
+    type Output<SomeNewAttr: Attribute> = T::Output<SomeNewAttr>;
 
-    fn add_any_attr<NewAttr: Attribute<Rndr>>(
+    fn add_any_attr<NewAttr: Attribute>(
         self,
         attr: NewAttr,
     ) -> Self::Output<NewAttr>
     where
-        Self::Output<NewAttr>: RenderHtml<Rndr>,
+        Self::Output<NewAttr>: RenderHtml,
     {
         self.into_inner().add_any_attr(attr)
     }
